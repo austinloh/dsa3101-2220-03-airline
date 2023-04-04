0d52039cbe005c1ff83837816324c89b48f03994
amandalian <amandalianhj@gmail.com>
2023-04-05 03:27:51 +0800

    nicer models page

--- frontend/pages/models.py
+++ frontend/pages/models.py
@@ -16,6 +16,10 @@ dash.register_page(__name__)
 
 #------ importing and pre processing of data -------------
 
+delay_yes = '../assets/delay_yes.png'
+delay_no = '../assets/delay_no.png'
+delay_or_not = '../assets/delay_or_not.png'
+
 # h1 = {'Content-type': 'application/json', 'Accept': 'application/json'}
 #['Month','DayofMonth', 'DayOfWeek', 'CRSDepTime', 'CRSArrTime', 'UniqueCarrier',
 #       'TailNum', 'CRSElapsedTime', 'Origin', 'Dest', 'Distance']
@@ -62,16 +66,15 @@ headers = {"Content-Type": "application/json"}
 # prediction = str(requests.post('http://127.0.0.1:5000/predict', data=json.dumps(input_data), headers=headers).json()['prediction'])
 
 
-
 def generate_pred(DayOfWeek, CRSDepTime, origin_state):
     input_dict = input_data.copy()
-    if DayOfWeek:
+    if DayOfWeek and CRSDepTime and origin_state:
         input_dict['DayOfWeek'] = DayOfWeek
-    if CRSDepTime:
         input_dict['CRSDepTime'] = CRSDepTime
-    if origin_state:
         input_dict['origin_state'] = origin_state
-    return str(requests.post('http://127.0.0.1:5000/predict', data=json.dumps(input_dict), headers=headers).json()['prediction'])
+        # '0' for no delay, '1' for delay
+        return str(requests.post('http://127.0.0.1:5000/predict', data=json.dumps(input_dict), headers=headers).json()['prediction'])
+    return '2' # '2' for no full user input yet
 
 
 #------- App layout ----------
@@ -79,41 +82,58 @@ def generate_pred(DayOfWeek, CRSDepTime, origin_state):
 layout = html.Div(children=[
     # html.H2(feature_importance),
     html.Br(),
-    # html.H2(prediction),
-    # dcc.DatePickerSingle(
-    #     id='year_month_date',
-    #     date=date(2006, 1, 11),
-    #     min_date_allowed=date(2006, 1, 1),
-    #     max_date_allowed=date(2008, 12, 31)
-    # ),
-    html.H1(id='prediction_text'),
-    dcc.Slider(
-        id='DayOfWeek',
-        min=1,
-        max=7,
-        marks={1:'Mon', 2:'Tue', 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sat', 7:'Sun'},
-        value=3,
-        # vertical=False
-    ),
-    dcc.Slider(
-        id='CRSDepTime',
-        min=0,
-        max=47,
-        marks={i: f"{i//2}{'30' if i%2 else '00'}" for i in range(48)},
-        value=22,
-        # vertical=False
-    ),
-    dcc.Dropdown(
-        id='origin_state',
-        options = ['AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'FL', 'GA', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY'],
-        style={'color': 'black'},
-    ) 
+    html.H1(id='prediction_text', style={'textAlign': 'center'}),
+    html.Div([
+        html.Img(id='prediction_img', style={"width": "70vw", "margin": "0 auto"})
+    ]),
+    html.Br(),
+    html.Br(),
+    dbc.Row([
+        html.H6("Flight Origin:", style={'textAlign': 'center'}),
+        html.Br(),
+        dcc.Dropdown(
+            id='origin_state',
+            options = ['AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'FL', 'GA', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY'],
+            style={'color': 'black'},
+            placeholder="Select a state..."
+        )
+    ]),
+    html.Br(),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.H6("Day of Week:", style={'textAlign': 'center'}),
+            dcc.Slider(
+                id='DayOfWeek',
+                min=1,
+                max=7,
+                marks={1:'Mon', 2:'Tue', 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sat', 7:'Sun'},
+                value=3,
+                vertical=True,
+            )
+        ]),
+        dbc.Col([
+            html.H6("Scheduled Departure Hour:", style={'textAlign': 'center'}),
+            dcc.Slider(
+                id='CRSDepTime',
+                min=0,
+                # max=47,
+                # marks={i: f"{('0'+str(i//2))[-2:]}:{'30' if i%2 else '00'}" for i in range(48)},
+                # value=22,
+                max=23,
+                marks={i: f"{('0'+str(i))[-2:]}:00" for i in range(24)},
+                value=11,
+                vertical=True
+            )
+        ])
+    ])
 ])
 
 # ----- Connect Plotly graphs with Dash Components -----
 
 @callback(
-    Output(component_id='prediction_text', component_property='children'),
+    [Output(component_id='prediction_text', component_property='children'),
+    Output(component_id='prediction_img', component_property='src')],
     [Input(component_id='DayOfWeek', component_property='value'),
     Input(component_id='CRSDepTime', component_property='value'),
     Input(component_id='origin_state', component_property='value')]
@@ -122,9 +142,14 @@ layout = html.Div(children=[
 def update_output(DayOfWeek, CRSDepTime, origin_state):
     pred = generate_pred(DayOfWeek, CRSDepTime, origin_state)
     if pred=='0':
-        return "No delay :)"
+        return "No delay!", delay_no
+        # return delay_no
+    elif pred=='1':
+        return "DELAY", delay_yes
+        # return delay_yes
     else:
-        return "DELAY"
+        return "Will the flight be delayed?", delay_or_not
+        # return delay_or_not
 
 
 # ---------------------------------------
